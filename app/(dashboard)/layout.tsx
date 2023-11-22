38b3aa8f89c0839efc43c1e1629c5d9164415662
Yogesh K <yogeshcodes@zohomail.in>
2023-11-22 14:56:19 +0530

    correct the grid layout for small devices(add col-1)

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import NavBar from '@/components/navbar'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid grid-cols-[12rem,1fr] h-screen">
+    <div className="grid grid-cols-1 md:grid-cols-[12rem,1fr] h-screen">
       <div className="hidden md:grid bg-gray-800">
         <Sidebar />
       </div>
